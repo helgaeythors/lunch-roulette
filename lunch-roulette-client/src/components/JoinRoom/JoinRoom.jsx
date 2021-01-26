695a788971e448b5b6e0044cde38dd6044da6017
Helga <helga17@ru.is>
2021-01-26 20:23:57 +0000

    Improve validation for joining a room and split the process into two steps

--- lunch-roulette-client/src/components/JoinRoom/JoinRoom.jsx
+++ lunch-roulette-client/src/components/JoinRoom/JoinRoom.jsx
@@ -12,8 +12,9 @@ class JoinRoom extends React.Component {
         this.state = {
             username: "",
             usernameError: false,
+            validUserName: false,
             shouldRedirect: false,
-            roomcode: null,
+            roomcode: "",
             roomcodeError: false,
         };
     }
@@ -21,35 +22,46 @@ class JoinRoom extends React.Component {
         const { name, value } = event.target;
         this.setState({ [name]: value });
     }
-    handleSubmit = (event) => {
-        event.preventDefault();
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const { validUserName } = this.state;
 
+        if (!validUserName) {
+            this.handleNewUser();
+        } else{
+            this.handleJoinRoom();
+        }
+    }
+    handleNewUser = () => {
         const { username } = this.state;
         const { socket } = this.props;
 
         socket.emit('newuser', username, (available) => {
-            const { roomcode } = this.state;
-
             if (available) {
                 // name avilable
-                this.setState({ usernameError: false });
-                socket.emit('joinroom', roomcode, (success) => {
-                    if (success) {
-                        this.setState({ roomcodeError: false, shouldRedirect: true });
-                    }
-                    else {
-                        this.setState({ roomcodeError: true });
-                    }
-                });
+                this.setState({ usernameError: false, validUserName: true });
             } else {
                 // name not available
-                this.setState({ usernameError: true });
+                this.setState({ usernameError: true, validUserName: false });
+            }
+        });
+    }
+    handleJoinRoom = () => {
+        const { roomcode } = this.state;
+        const { socket } = this.props;
+
+        socket.emit('joinroom', roomcode, (success) => {
+            if (success) {
+                this.setState({ roomcodeError: false, shouldRedirect: true });
+            }
+            else {
+                this.setState({ roomcodeError: true, shouldRedirect: false });
             }
         });
     }
     render() {
         const { classes } = this.props;
-        const { username, usernameError, roomcode, roomcodeError, shouldRedirect } = this.state;
+        const { username, usernameError, validUserName, roomcode, roomcodeError, shouldRedirect } = this.state;
 
         if (shouldRedirect) {
             return <Redirect to={{
@@ -62,35 +74,53 @@ class JoinRoom extends React.Component {
             <div className="JoinRoom-container">
                 <form onSubmit={this.handleSubmit}>
                     <div className="JoinRoom-form">
-                        <TextField id="JoinRoom-textfield-roomcode"
-                            name="roomcode"
-                            label="Enter the room code..."
-                            variant="filled"
-                            color="primary"
-                            className={classes.margin}
-                            onChange={this.handleChangeInput}
-                            error={roomcodeError}
-                            helperText={roomcodeError ? "Room not available" : ""}
-                        />
-                        <TextField id="JoinRoom-textfield-username"
-                            name="username"
-                            label="Enter your name..."
-                            variant="filled"
-                            color="primary"
-                            className={classes.margin}
-                            onChange={this.handleChangeInput}
-                            error={usernameError}
-                            helperText={usernameError ? "Name unavailable" : ""}
-                        />
-                        <Button
-                            variant="contained"
-                            color="primary"
-                            className={classes.margin}
-                            type="submit"
-                            disabled={username === ""}
-                        >
-                            Join
-                        </Button>
+                        {!validUserName ?
+                        <>
+                            <TextField id="JoinRoom-textfield-username"
+                                name="username"
+                                label="Enter your name..."
+                                value={username}
+                                variant="filled"
+                                color="primary"
+                                className={classes.margin}
+                                onChange={this.handleChangeInput}
+                                error={usernameError}
+                                helperText={usernameError ? "Name unavailable" : ""}
+                            />
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                className={classes.margin}
+                                disabled={username === ""}
+                                onClick={this.handleNewUser}
+                                type="button"
+                            >
+                                Continue
+                            </Button>
+                        </> :
+                        <>
+                            <TextField id="JoinRoom-textfield-roomcode"
+                                name="roomcode"
+                                label="Enter the room code..."
+                                value={roomcode}
+                                variant="filled"
+                                color="primary"
+                                className={classes.margin}
+                                onChange={this.handleChangeInput}
+                                error={roomcodeError}
+                                helperText={roomcodeError ? "Room not available" : ""}
+                            />
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                className={classes.margin}
+                                disabled={username === ""}
+                                onClick={this.handleJoinRoom}
+                                type="button"
+                            >
+                                Join
+                            </Button>
+                        </>}
                     </div>
                 </form>
             </div>
